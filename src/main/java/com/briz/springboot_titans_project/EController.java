81086704eae2fe6297aeed8a386a5c3c2f99b3b5
navin1542 <gamernavinbhai@gmail.com>
2026-04-08 11:16:38 +0530

    this is my new end point

--- src/main/java/com/briz/springboot_titans_project/EController.java
+++ src/main/java/com/briz/springboot_titans_project/EController.java
@@ -1,25 +1,15 @@
 package com.briz.springboot_titans_project;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EController {
- 
-@RequestMapping("/Mukesh")
-public String getMukesh() {
-	return "Hello Mukesh";
-}
-}
-	@Autowired
-	ERepository ERepository;
-	
-	@RequestMapping("/save")
-	public String addEmployee(@RequestBody Employee employee) {
-		ERepository.save(employee);
-		return "Employee added successfully";
-	}
 
+public class EController {
+
+
+	@RequestMapping("/hello Navin")
+	public String hello() {
+		return "Hello, Navin!";
+	}
 }
