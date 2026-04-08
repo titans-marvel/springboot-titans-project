6e4701c957e64b4ace2a332691497888ca6d49a3
Nitin <nitinkumarmahli3@gmail.com>
2026-04-09 00:40:03 +0530

    i have updated the controller and tested the project endpoints in postman

--- src/main/java/com/briz/springboot_titans_project/EController.java
+++ src/main/java/com/briz/springboot_titans_project/EController.java
@@ -1,6 +1,10 @@
 package com.briz.springboot_titans_project;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class EController {
 	@Autowired
 	ERepository ERepository;
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
 	
-	@RequestMapping("/save")
+	@PostMapping("/save")
 	public String addEmployee(@RequestBody Employee employee) {
 		ERepository.save(employee);
 		return "Employee added successfully";
+	}
+	
+	@GetMapping("/getAll")
+	public List<Employee> getAllEmployees() {
+		return ERepository.findAll();
 	}
 
 }
