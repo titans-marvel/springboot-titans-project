d6ad389f153fd1dda01430f875c4c003bd3df78c
kanchan raju <kanchanraju2015@gmail.com>
2026-04-08 10:00:00 +0530

    first titans commit

---
+++ src/main/java/com/briz/springboot_titans_project/SpringbootTitansProjectApplication.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_titans_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTitansProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTitansProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briz/springboot_titans_project/SpringbootTitansProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_titans_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTitansProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
